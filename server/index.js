8f22ea557da04bf41e7bc5edba51a7effbdc4240
Aditi-Wanjari06 <aditiWan2003@gmail.com>
2024-07-25 21:56:57 +0530

    Backend is ready

--- server/index.js
+++ server/index.js
@@ -8,26 +8,37 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
-const connectDB = async ()=>{
+import { postLogin, postSignup } from "./controllers/user.js";
+import { getTransactions, postTransaction } from "./controllers/transaction.js";
+
+
+
+const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URL)
 
-    if(conn){
+    if (conn) {
         console.log("MongoDB connected successfully✅")
     }
 };
 connectDB();
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
         message: "Welcome to Expense Tracker API"
     })
 })
 
+app.post("/signup", postSignup)
+app.post("/login", postLogin)
+
+app.post("/transaction", postTransaction)
+app.get("/transactions", getTransactions)
+
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
